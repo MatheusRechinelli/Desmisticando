f764090d63c78024628de832165813ad840af7bf
Matheus Rechinelli <matheus_rechinelli@outlook.com>
2022-05-18 21:51:10 -0400

    work_24

---
+++ work_24/js/scripts.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector("#send");
+
+btn.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    const name = document.querySelector("#name");
+
+    const value = name.value;
+
+    console.log(value);
+
+    return alert(value);
+});
